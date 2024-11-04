557bcaf0558d8125b6f2c261bcaec8ffedea5d4c
0xhimangshu <saikiahimangshu1256@gmail.com>
2024-11-05 02:25:33 +0530

    oh yeaah

--- main.test.py
+++ main.test.py
@@ -2,19 +2,19 @@ import asyncio
 import json
 
 from saavn import Saavn
+from typing import List
 
 
 async def main(search:str):
     saavn = Saavn()
-    resp = await saavn.search_tracks(search, pages=1, count=1)
-    for song in resp:
-        print(song.title, song.media_url)
-    # print(len(resp))
-
-
+    resp = await saavn.get_track(search)
+    with open(f"test.mp3", "wb") as f:
+        f.write(await saavn.get_buffer(resp))
+ 
 def test():
     search = input("Search: ")
     asyncio.run(main(search=search))
 
 if __name__ == "__main__":
     test()
+    # unnecessary
